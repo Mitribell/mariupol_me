284306852e21fef921e35c81aa31c0e6634145b0
Dmytro <Mitribell@gmail.com>
2023-08-09 23:15:18 +0300

    add image to popup box

--- mapbox.js
+++ mapbox.js
@@ -1,22 +1,26 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoibWl0cmliZWxsIiwiYSI6ImNsa254dndtMjA1OHIzZGs1dTE5ZWYzdDIifQ.TWdYke6lFIgEtk3KZAaHrg';
  
 const map = new mapboxgl.Map({
-container: 'map',
-style: 'mapbox://styles/mitribell/clkp04typ003q01qx6seodzc7'
+    container: 'map',
+    style: 'mapbox://styles/mitribell/clkp04typ003q01qx6seodzc7'
 });
+
 map.on('click', (event) => {
-const features = map.queryRenderedFeatures(event.point, {
-layers: ['symbols']
+    const features = map.queryRenderedFeatures(event.point, {
+    layers: ['symbols']
 });
+
 if (!features.length) {
-return;
+    return;
 }
+
 const feature = features[0];
  
 const popup = new mapboxgl.Popup({ offset: [0, -15] })
 .setLngLat(feature.geometry.coordinates)
 .setHTML(
-`<h3><a href = '${feature.properties.URL}'>${feature.properties.Name}</a></h3>`
+    `<img src='image/${feature.properties.Image}' />
+    <h2><a href = '${feature.properties.URL}'>${feature.properties.Name}</a></h2>`
 )
 .addTo(map);
 });
